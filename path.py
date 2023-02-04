25ea7dd1e8f4e82af354575be33f47cb08eeb767
Michał Pelc <michal.pelc@live.com>
2023-02-04 18:01:01 +0100

    Comments

--- path.py
+++ path.py
@@ -4,12 +4,15 @@ import pygame
 import math
 from queue import PriorityQueue
 
+# Setting the windows dimensions
 WIDTH = 800
 HEIGHT = 800
 
+# Inizializing the main window
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("A* Pathfinding Algorithm Visualizer")
 
+# Defining basic colors
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
